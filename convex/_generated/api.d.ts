9a801dc8a693e1e8bef953c314458f2f63774708
Austin Chen <akrolsmir@gmail.com>
2026-09-04 12:52:03 -0700

    Format

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,11 +13,7 @@ import type * as feeds from "../feeds.js";
 import type * as processing from "../processing.js";
 import type * as slugs from "../slugs.js";
 
-import type {
-  ApiFromModules,
-  FilterApi,
-  FunctionReference,
-} from "convex/server";
+import type { ApiFromModules, FilterApi, FunctionReference } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
   episodes: typeof episodes;
@@ -34,10 +30,7 @@ declare const fullApi: ApiFromModules<{
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-export declare const api: FilterApi<
-  typeof fullApi,
-  FunctionReference<any, "public">
->;
+export declare const api: FilterApi<typeof fullApi, FunctionReference<any, "public">>;
 
 /**
  * A utility for referencing Convex functions in your app's internal API.
@@ -47,9 +40,6 @@ export declare const api: FilterApi<
  * const myFunctionReference = internal.myModule.myFunction;
  * ```
  */
-export declare const internal: FilterApi<
-  typeof fullApi,
-  FunctionReference<any, "internal">
->;
+export declare const internal: FilterApi<typeof fullApi, FunctionReference<any, "internal">>;
 
 export declare const components: {};
